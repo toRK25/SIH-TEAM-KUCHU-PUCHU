b4b6520c846c7b3706401cb137d05f234902b961
JALAJ <jalajgoel08@gmail.com>
2026-09-14 23:21:02 +0530

    Update Prototype.tsx

--- Karigar-Kart-Profile-Updated-v9/src/Prototype.tsx
+++ Karigar-Kart-Profile-Updated-v9/src/Prototype.tsx
@@ -1,3 +1,5 @@
+import { createArtisanProfile } from './lib/profileservices';
+import { supabase } from './lib/supabase';
 import { addProductToSupabase, uploadProductImage, ProductInput } from './lib/productservices';
 import { useEffect, useRef, useState, type ReactNode } from 'react';
 import { Microphone, Camera, VideoCamera, Storefront, House, Bag, ChatCircleDots, User, Globe, Bell, Package, ChartBar, ArrowLeft, ArrowRight, CaretRight, Check, Plus, PencilSimple, Stop, UploadSimple, Sun, Crosshair, Image as ImageIcon, SignOut, Question, X, CheckCircle, Eye, Sparkle, QrCode, ChatText, Info, Phone, LockKey } from '@phosphor-icons/react';
